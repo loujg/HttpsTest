40d7fe798ad82d49efc51cd09371688dbb992b2c
xiaoyuancai <yuanle@hudong.com>
2017-01-04 16:33:28 +0800

    Initial Commit

---
+++ HttpsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsTest
+//
+//  Created by 互动 on 2017/1/4.
+//  Copyright © 2017年 互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpsTest
+//
+//  Created by 互动 on 2017/1/4.
+//  Copyright © 2017年 互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
